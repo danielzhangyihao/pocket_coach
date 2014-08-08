7aad2a5e879ceafbee9c351c3196cdf606c09b93
daniel <zhangyihao1009@gmail.com>
2014-08-08 12:43:28 -0700

    style for instructor and company page

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -1,7 +1,7 @@
 module InstructorsHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(instructor)
+  def gravatar_for(instructor, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(instructor.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: instructor.name, class: "gravatar")
